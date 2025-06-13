822c71c4a8c5a69447298845f7fd3b6af1de80be
vasishak <vasishak.d@gmail.com>
2025-06-13 21:05:04 +0500

    add front

--- src/schemas/web.py
+++ src/schemas/web.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel
 
 class LoginTelegramParams(BaseModel):
     id: int
-    first_name: str
-    last_name: str
-    username: str
-    photo_url: str
+    first_name: str | None = None
+    last_name: str | None = None
+    username: str | None = None
+    photo_url: str | None = None
     auth_date: int
     hash: str
